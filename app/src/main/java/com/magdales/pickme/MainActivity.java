66244b2239b054d005fc4f2bbf2993284c1b4db9
Alyssa Marie Magdales <magdalesalyssa@yahoo.com>
2017-08-18 00:30:51 +0800

    Implementation of all functions

--- app/src/main/java/com/magdales/pickme/MainActivity.java
+++ app/src/main/java/com/magdales/pickme/MainActivity.java
@@ -1,10 +1,7 @@
 package com.magdales.pickme;
 
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
@@ -12,61 +9,61 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.Toast;
-
-import java.io.FileNotFoundException;
+import android.widget.TextView;
 import java.io.IOException;
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static int PICK_IMAGE_REQUEST = 1;
+    private static int PICK_PHOTO_REQUEST = 1;
 
-    Button shareBtn = (Button) findViewById(R.id.share_button);
+    ImageView viewedImg;
+    TextView pickText;
+    Button shareBtn;
+    Uri uri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageView choosePhoto = (ImageView) findViewById(R.id.camera_photo);
-        choosePhoto.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(i, PICK_IMAGE_REQUEST);
-            }
-        });
+        findViews();
     }
 
-    /*
-    Intent intent = new Intent();
-// Show only images, no videos or anything else
-intent.setType("image/*");
-intent.setAction(Intent.ACTION_GET_CONTENT);
-// Always show the chooser (if there are multiple options available)
-startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    public void findViews() {
+        viewedImg = (ImageView) findViewById(R.id.viewed_photo);
+        pickText = (TextView) findViewById(R.id.camera_text);
+        shareBtn = (Button) findViewById(R.id.share_button);
+    }
 
-     */
+    public void pickPhoto(View v) {
+        Intent gallery = new Intent(Intent.ACTION_PICK);
+        gallery.setType("image/*");
+        startActivityForResult(gallery, PICK_PHOTO_REQUEST);
+    }
+
+    public void sharePhoto(View v) {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("image/*");
+        share.setData(uri);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        startActivity(Intent.createChooser(share, getString(R.string.share_text_)));
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
-            Uri uri = data.getData();
-
+        if (requestCode == PICK_PHOTO_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            uri = data.getData();
             try {
                 Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
-                // Log.d(TAG, String.valueOf(bitmap));
-
-                ImageView imageView = (ImageView) findViewById(R.id.viewed_photo);
-                imageView.setImageBitmap(bitmap);
-                imageView.setVisibility(View.VISIBLE);
+                viewedImg.setImageBitmap(bitmap);
+                viewedImg.setVisibility(View.VISIBLE);
+                shareBtn.setVisibility(View.VISIBLE);
+                pickText.setVisibility(View.INVISIBLE);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-
-
     }
 }
